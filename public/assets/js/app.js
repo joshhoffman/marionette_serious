5e067c87694c9ca0d3d9969a714358442d16233c
joshhoffman <josh.edward.hoffman@gmail.com>
2014-08-24 21:51:11 -0500

    initial commit

---
+++ public/assets/js/app.js
@@ -0,0 +1,36 @@
+var ContactManager = new Marionette.Application();
+
+ContactManager.addRegions({
+  headerRegion: "#header-region",
+  mainRegion: "#main-region",
+  dialogRegion: Marionette.Region.Dialog.extend({
+    el: "#dialog-region"
+  })
+});
+
+ContactManager.navigate = function(route,  options){
+  options || (options = {});
+  Backbone.history.navigate(route, options);
+};
+
+ContactManager.getCurrentRoute = function(){
+  return Backbone.history.fragment
+};
+
+ContactManager.on("before:start", function() {
+    _.templateSettings = {
+        interpolate: /\{\{= (.+?)\}\}/g,
+        escape: /\{\{= (.+?)\}\}/g,
+        evaluate: /\{\{(.+?)\}\}/g
+    };
+});
+
+ContactManager.on("start", function(){
+  if(Backbone.history){
+    Backbone.history.start();
+
+    if(this.getCurrentRoute() === ""){
+      ContactManager.trigger("contacts:list");
+    }
+  }
+});
---
+++ public/assets/js/entities/contact.js
@@ -0,0 +1,70 @@
+ContactManager.module("Entities", function(Entities, ContactManager, Backbone, Marionette, $, _){
+    Entities.Contact = Backbone.Model.extend({
+        urlRoot: "contacts",
+
+        defaults: {
+            firstName: "",
+            lastName: "",
+            phoneNumber: ""
+        },
+
+        validate: function(attrs, options) {
+            var errors = {}
+            if (! attrs.firstName) {
+                errors.firstName = "can't be blank";
+            }
+            if (! attrs.lastName) {
+                errors.lastName = "can't be blank";
+            }
+            else{
+                if (attrs.lastName.length < 2) {
+                    errors.lastName = "is too short";
+                }
+            }
+            if( ! _.isEmpty(errors)){
+                return errors;
+            }
+        }
+    });
+
+    Entities.ContactCollection = Backbone.Collection.extend({
+        url: "contacts",
+        model: Entities.Contact,
+        comparator: "firstName"
+    });
+
+    var API = {
+        getContactEntities: function() {
+            var contacts = new Entities.ContactCollection();
+            var defer = $.Deferred();
+            contacts.fetch({
+                success: function (data) {
+                    defer.resolve(data);
+                }
+            });
+            return defer.promise();
+        },
+
+        getContactEntity: function(contactId){
+            var contact = new Entities.Contact({id: contactId});
+            var defer = $.Deferred();
+            contact.fetch({
+                success: function(data){
+                    defer.resolve(data);
+                },
+                error: function(data){
+                    defer.resolve(undefined);
+                }
+            });
+            return defer.promise();
+        }
+    };
+
+    ContactManager.reqres.setHandler("contact:entities", function(){
+        return API.getContactEntities();
+    });
+
+    ContactManager.reqres.setHandler("contact:entity", function(id){
+        return API.getContactEntity(id);
+    });
+});
